79c76e72d1e1be8e600636b9914860637f5c0dbe
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2024-07-31 23:29:06 -0300

    Update Routes.php

--- Trabalho Farmacia/app/Config/Routes.php
+++ Trabalho Farmacia/app/Config/Routes.php
@@ -8,3 +8,4 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/cadastro', 'Home::cadastro');
 $routes->post('/cadastrar', 'Home::cadastrar');
+$routes->post('/pesquisar', 'Home::pesquisar');
