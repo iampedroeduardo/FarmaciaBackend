4e2fe741d2db49f7ebb36bfe803d292d6ae1d224
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2024-07-31 23:28:19 -0300

    Update inicio.php

--- Trabalho Farmacia/app/Views/inicio.php
+++ Trabalho Farmacia/app/Views/inicio.php
@@ -6,8 +6,49 @@
     <title>Farmácia</title>
 </head>
 <body>
-    <form action="cadastro" method="get">
-        <button>Cadastrar Medicamento</button>
-    </form>
+    <header>
+        <form action="pesquisar" method="post">
+            <input type="search" name="pesquisa">
+            <select name="campo">
+                <option value="nome">Nome</option>
+                <option value="laboratorio">Laboratório</option>
+            </select>
+            <select name="ordem">
+                <option value="preco">Preço</option>
+                <option value="quantidade">Quantidade</option>
+            </select>
+            <button type="submit">Pesquisar</button>
+        </form>
+    </header>
+    <div class="cadastro">
+        <form action="cadastro" method="get">
+            <button type="submit">Cadastrar Medicamento</button>
+        </form>
+    </div>
+    <div class="medicamentos">
+        <table>
+            <thead>
+                <tr>
+                    <td>Nome</td>
+                    <td>Laboratório</td>
+                    <td>Preço</td>
+                    <td>Quantidade</td>
+                </tr>
+            </thead>
+        <?php 
+            for($i = 0; $i < count($dados); $i++){
+                $medicamento = $dados[$i];
+        ?>
+            <tr>
+                <td><?=$medicamento['nome']?></td>
+                <td><?=$medicamento['laboratorio']?></td>
+                <td><?=$medicamento['preco']?></td>
+                <td><?=$medicamento['quantidade']?></td>
+            </tr>
+        <?php
+            }
+        ?>
+        </table>
+    </div>
 </body>
 </html>
